f9a3fd1a8a0a3919ff7a97b1fe882cef84d969a4
Ivailo Hristov <crownandthering@gmail.com>
2018-01-30 00:21:04 +0200

    Add support for local configs not tracked by git

--- config.php
+++ config.php
@@ -1,30 +1,36 @@
 <?php
-$server_config = array(
-    65000 => array(
-        'WebSocket' => array(
-            'hosts' => array(
-                'localhost' => array('WebChat', 'SimpleEcho'),
+$localConfig = __DIR__ . "/config_local.php";
+
+if (file_exists($localConfig)) {
+    include $localConfig;
+} else {
+    $server_config = array(
+        65000 => array(
+            'WebSocket' => array(
+                'hosts' => array(
+                    'localhost' => array('WebChat', 'SimpleEcho'),
+                )
+            ),
+            'ssl' => array(
+                'cert_file' => '',
+                'privkey_file' => '',
+                'passphrase' => ''
             )
         ),
-        'ssl' => array(
-            'cert_file' => '',
-            'privkey_file' => '',
-            'passphrase' => ''
-        )
-    ),
-    65001 => array(
-        'RawTcp' => array()
-    ),
-    65002 => array(
-        'Http' => array(
-            'hosts' => array(
-                'localhost' => array('HelloWorld', 'Maintenance'),
-            )
+        65001 => array(
+            'RawTcp' => array()
         ),
-        'ssl' => array(
-            'cert_file' => '',
-            'privkey_file' => '',
-            'passphrase' => ''
+        65002 => array(
+            'Http' => array(
+                'hosts' => array(
+                    'localhost' => array('HelloWorld', 'Maintenance'),
+                )
+            ),
+            'ssl' => array(
+                'cert_file' => '',
+                'privkey_file' => '',
+                'passphrase' => ''
+            )
         )
-    )
-);
+    );
+}
