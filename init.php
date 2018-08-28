553193b3017f177247df35f9694a510090fa915b
quantuumsnot <quantuumsnot@users.noreply.github.com>
2018-08-28 15:20:47 +0300

    Small update on how we get the number of cpu cores
    Changed the code a bit for better readability and future improvements

--- init.php
+++ init.php
@@ -9,28 +9,28 @@ define('DIR_LOG', DIR_ROOT . 'logs');
 define('DEBUG_MODE', TRUE);
 
 //limit the number of forked processes to the number of available cpu cores
-function cpu_cores() {
-  $cores = 1;
-  if (is_file('/proc/cpuinfo')) {
-    $cpuinfo = file_get_contents('/proc/cpuinfo');
-    preg_match_all('/^processor/m', $cpuinfo, $matches);
-    $cores = count($matches[0]);
-  } else if ('WIN' == strtoupper(substr(PHP_OS, 0, 3))) {
-    $process = popen('wmic cpu get NumberOfLogicalProcessors', 'rb');
-    if (false !== $process) {
-      fgets($process);
+function cpu_cores($cores = 1) {
+  $OS = strtoupper(substr(PHP_OS, 0, 3));
+  
+  if ('WIN' == $OS) {
+    //TODO: test 'the wmic cpu get' on different Windowses (tested only on Win7 x64)
+    //NOTE: on Windows XP x64 the command is 'echo %NUMBER_OF_PROCESSORS%'
+    if (fgets($process = popen('wmic cpu get NumberOfLogicalProcessors', 'rb'))) {
       $cores = intval(fgets($process));
       pclose($process);
     }
-  } else {
-    $process = popen('sysctl -a', 'rb');
-    if (false !== $process) {
-      $output = stream_get_contents($process);
-      preg_match('/hw.ncpu: (\d+)/', $output, $matches);
+  } else if ('LIN' == $OS) { //tested on Ubuntu x64 14.04, 16.04, 18.04 and Slack x64 14.1
+    if (is_file('/proc/cpuinfo')) {
+      preg_match_all('/^processor/m', file_get_contents('/proc/cpuinfo'), $matches);
+      $cores = count($matches[0]);
+    } else if ($process = popen('sysctl -a', 'rb')) {
+      preg_match('/hw.ncpu: (\d+)/', stream_get_contents($process), $matches);
       if ($matches) { $cores = intval($matches[1][0]); }
-      pclose($process);
-    }
-  }
+    } else { echo "Unknown Linux-based operating system, setting the fork limit to 1 ... "; }
+  } else { echo "Unknown operating system, setting the fork limit to 1 ... "; }
+  
+  //enable only for testing
+  //return "{$OS} | {$cores}";
   
   return $cores;
 }
